f9e6c6aa1f30ccad01107ae1ccb133e99a529d67
Agustin Wisky <agustinwisky@MacBook-de-Daniel.local>
2019-10-29 15:37:25 -0300

    [ADD][custom_ed]

---
+++ custom_ed/__manifest__.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2019  Calyx  (http://www.calyxservicios.com.ar)
+#    All Rights Reserved.
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Custom ED',
+    'version': '11.0.1',
+    'category': 'Tools',
+    'author': "Calyx",
+    'website': 'www.calyxservicios.com.ar',
+    'license': 'AGPL-3',
+    'summary': '''Eventos Distintos Customization''',
+    'depends': [
+        'base',
+        'calendar',
+        'crm',
+        'point_of_sale'
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'data/ir_sequence_data.xml',
+        'view/calendar_view.xml',
+        'view/lead_view.xml',
+        'view/color_view.xml',
+        'view/res_partner_view.xml',
+        'view/pos_session_view.xml',
+        'report/paperformat.xml',
+        'report/pos_report.xml',
+        'report/pos_template.xml',
+        'report/pos_session_report.xml',
+        'report/pos_session_template.xml',
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ custom_ed/models/__init__.py
@@ -0,0 +1,9 @@
+##############################################################################
+# For copyright and license notices, see __manifest__.py file in module root
+# directory
+##############################################################################
+
+from . import calendar
+from . import lead
+from . import res_partner
+from . import pos_session
