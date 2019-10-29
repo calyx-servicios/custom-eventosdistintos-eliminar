199178257341afa04269026d808c6d23525e4fc5
Agustin Wisky <agustinwisky@MacBook-de-Daniel.local>
2019-10-29 15:38:52 -0300

    [ADD][custom_ed]

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import calendar
-from . import lead
-from . import res_partner
-from . import pos_session
