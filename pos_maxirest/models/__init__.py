0b80c9eaeed00b33edb73536df41034a334fb70b
Agustin Wisky <agustinwisky@MacBook-de-Daniel.local>
2019-11-14 15:36:29 -0300

    [DEL][calendar_colors|payment_terms|pos_maxirest]

--- pos_maxirest/models/__init__.py
+++
@@ -1,7 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-
-from . import pos_session
-from . import pos_order
