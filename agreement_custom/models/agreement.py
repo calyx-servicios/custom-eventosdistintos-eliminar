1034cc218b9479ef3a035a68161165b7a14661d5
Jhone Mendez <jhone0901@gmail.com>
2020-06-10 13:01:09 -0300

    [UPD] fix button agreement custom

--- agreement_custom/models/agreement.py
+++ agreement_custom/models/agreement.py
@@ -1,37 +1,33 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
+from odoo import api, fields, models
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 
 import datetime
 import pytz
 from dateutil import tz
-from pytz import timezone
-from dateutil.relativedelta import relativedelta
-
-from odoo import SUPERUSER_ID
 
 import logging
+
 _logger = logging.getLogger(__name__)
 
 # Main Agreement Section Records Model
 
 
 class AgreementStage(models.Model):
-    _inherit = 'agreement.stage'
+    _inherit = "agreement.stage"
 
     close_stage = fields.Boolean(
-        string="Is Close Stage",
-        help="This sets this stage as the Close stage"
+        string="Is Close Stage", help="This sets this stage as the Close stage"
     )
 
 
 class Agreement(models.Model):
-    _inherit = 'agreement'
+    _inherit = "agreement"
 
-    # # compute the dynamic content for mako expression inside a try in case the user fuck it
+    # # compute the dynamic content for mako expression inside a try in
+    # case the user fuck it
     # @api.multi
     # @api.onchange('start_date','end_date','sign_date','description','sale_id','sale_id.partner')
     # def _compute_dynamic_description(self):
@@ -42,51 +38,58 @@ class Agreement(models.Model):
 
     description = fields.Html(
         string="Description",
-        track_visibility='onchange',
-        help="Description of the agreement",)
+        track_visibility="onchange",
+        help="Description of the agreement",
+    )
 
     dynamic_description = fields.Html(
         compute="_compute_dynamic_description",
         string="Dynamic Description",
-        help='Compute dynamic description')
+        help="Compute dynamic description",
+    )
 
     start_date = fields.Datetime(
         string="Start Date",
-        track_visibility='onchange',
-        help="When the agreement starts.")
+        track_visibility="onchange",
+        help="When the agreement starts.",
+    )
     end_date = fields.Datetime(
         string="End Date",
-        track_visibility='onchange',
-        help="When the agreement ends.")
+        track_visibility="onchange",
+        help="When the agreement ends.",
+    )
 
     sign_date = fields.Date(
         string="Sign Date",
-        track_visibility='onchange',
-        help="When the agreement must be signed.")
+        track_visibility="onchange",
+        help="When the agreement must be signed.",
+    )
 
-    sale_id = fields.Many2one('sale.order', string='Sales Order')
+    sale_id = fields.Many2one("sale.order", string="Sales Order")
 
     def convert_date(self, date_to_convert):
         """
-        Function to convert from stored UTC Odoo default timezone to user timezone
+        Function to convert from stored UTC Odoo default
+         timezone to user timezone
         """
 
-        timezone = pytz.timezone(self._context.get('tz') or 'UTC')
-        #utc_zone = tz.gettz('UTC')
-        utc_timezone = pytz.timezone('UTC')
+        timezone = pytz.timezone(self._context.get("tz") or "UTC")
+        # utc_zone = tz.gettz('UTC')
+        utc_timezone = pytz.timezone("UTC")
 
         # to_zone based on tz variable defined on context
         to_zone = tz.gettz(str(timezone))
 
         utc_date = datetime.datetime.strptime(
-            date_to_convert, DEFAULT_SERVER_DATETIME_FORMAT)
+            date_to_convert, DEFAULT_SERVER_DATETIME_FORMAT
+        )
         utc_date = utc_timezone.localize(utc_date)
         converted_date = utc_date.astimezone(to_zone)
         return converted_date
 
     @api.multi
     def get_event_name(self):
-        name = 'Evento'
+        name = "Evento"
         if self.sale_id and self.sale_id.opportunity_id:
             name = self.sale_id.opportunity_id.name
         return name
@@ -118,5 +121,5 @@ class Agreement(models.Model):
     @api.multi
     def get_sign_date(self):
         if self.sign_date:
-            sign = datetime.datetime.strptime(self.sign_date, '%Y-%m-%d')
+            sign = datetime.datetime.strptime(self.sign_date, "%Y-%m-%d")
             return sign.strftime("%d de %B de %Y")
--- agreement_custom/models/sale_order.py
+++ agreement_custom/models/sale_order.py
@@ -1,74 +1,86 @@
 # Copyright (C) 2019 - TODAY, Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from num2words import num2words
-
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT, DEFAULT_SERVER_DATE_FORMAT
-import datetime
-import pytz
-
 import logging
+
 _logger = logging.getLogger(__name__)
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     agreement_template_id = fields.Many2one(
-        'agreement',
+        "agreement",
         string="Agreement Template",
-        domain="[('is_template', '=', True)]")
+        domain="[('is_template', '=', True)]",
+    )
 
-    agreement_id = fields.Many2one('agreement', string="Agreement", copy=False)
+    agreement_id = fields.Many2one("agreement", string="Agreement", copy=False)
     start_date = fields.Datetime(
-        string="Start Date",
-        help="When the agreement starts.")
+        string="Start Date", help="When the agreement starts."
+    )
     end_date = fields.Datetime(
-        string="End Date",
-        help="When the agreement ends.")
+        string="End Date", help="When the agreement ends."
+    )
 
     sign_date = fields.Date(
-        string="Sign Date",
-        help="When the agreement must be signed.")
+        string="Sign Date", help="When the agreement must be signed."
+    )
 
     dynamic_description = fields.Html(
-        string="Dynamic Description",
-        help='Compute dynamic description')
+        string="Dynamic Description", help="Compute dynamic description"
+    )
 
     @api.multi
-    @api.depends('agreement_id', 'agreement_id.stage_id')
+    @api.depends("agreement_id", "agreement_id.stage_id")
     def _onchange_stage(self):
 
         for sale in self:
 
             close = False
             if sale.agreement_id:
-                _logger.debug('_onchange_stage====>%s',
-                              self.agreement_id.stage_id)
-                stage = self.env['agreement.stage'].search(
-                    [('close_stage', '=', True)])
+                _logger.debug(
+                    "_onchange_stage====>%s", self.agreement_id.stage_id
+                )
+                stage = self.env["agreement.stage"].search(
+                    [("close_stage", "=", True)]
+                )
                 if stage:
+
                     stage = stage and stage[0]
                     if self.agreement_id.stage_id.id == stage.id:
                         close = True
             sale.close = close
 
-    close = fields.Boolean(
-        string="Agreement Close", compute='_onchange_stage')
+    close = fields.Boolean(string="Agreement Close", compute="_onchange_stage")
 
     @api.multi
-    @api.onchange('start_date', 'end_date', 'sign_date', 'agreement_id', 'partner_id', 'name', 'amount_total', 'invoice_ids', 'invoice_ids.state', 'opportunity_id')
+    @api.onchange(
+        "start_date",
+        "end_date",
+        "sign_date",
+        "agreement_id",
+        "partner_id",
+        "name",
+        "amount_total",
+        "invoice_ids",
+        "invoice_ids.state",
+        "opportunity_id",
+    )
     def _onchange_dynamic(self):
         for sale in self:
             if sale.agreement_id:
                 sale.agreement_id.start_date = sale.start_date
                 sale.agreement_id.end_date = sale.end_date
                 sale.agreement_id.sign_date = sale.sign_date
-                sale.dynamic_description = sale.agreement_id.dynamic_description
+                sale.dynamic_description = (
+                    sale.agreement_id.dynamic_description
+                )
 
     @api.multi
-    @api.onchange('opportunity_id')
+    @api.onchange("opportunity_id")
     def _onchange_opportunity(self):
         for sale in self:
             if sale.opportunity_id:
@@ -78,41 +90,51 @@ class SaleOrder(models.Model):
                 sale.end_date = requested_date
 
     @api.multi
-    @api.depends('payment_term_id', 'amount_total', 'invoice_ids', 'invoice_ids.state')
+    @api.depends(
+        "payment_term_id", "amount_total", "invoice_ids", "invoice_ids.state"
+    )
     def _onchange_payment_term(self):
         for sale in self:
             if sale.payment_term_id and sale.payment_term_id.plan:
-                split = sale.payment_term_id.plan_split
+                # split = sale.payment_term_id.plan_split
                 invoiced = 0.0
                 for invoice in sale.invoice_ids:
-                    if invoice.state not in ('draft', 'canceled'):
+                    if invoice.state not in ("draft", "canceled"):
                         invoiced += invoice.amount_total
                 if sale.payment_term_id:
                     sale.sign_amount = invoiced
                     sale.remain_amount = sale.amount_total - sale.sign_amount
                     if sale.amount_total > 0:
                         sale.sign_percentage = round(
-                            100 * (sale.sign_amount / sale.amount_total), 2)
+                            100 * (sale.sign_amount / sale.amount_total), 2
+                        )
                         sale.remain_percentage = 100 - sale.sign_percentage
-                        words = num2words(sale.amount_total, lang='es')
+                        words = num2words(sale.amount_total, lang="es")
                         sale.amount_words = words.upper()
-                        words = num2words(sale.sign_amount, lang='es')
+                        words = num2words(sale.sign_amount, lang="es")
                         sale.sign_words = words.upper()
 
     sign_amount = fields.Monetary(
-        string='Sign Amount', compute=_onchange_payment_term)
+        string="Sign Amount", compute=_onchange_payment_term
+    )
     remain_amount = fields.Monetary(
-        string='Remain Amount', compute=_onchange_payment_term)
+        string="Remain Amount", compute=_onchange_payment_term
+    )
     sign_percentage = fields.Float(
-        string='Sign Percentage', compute=_onchange_payment_term)
+        string="Sign Percentage", compute=_onchange_payment_term
+    )
     remain_percentage = fields.Float(
-        string='Remain Percentage', compute=_onchange_payment_term)
+        string="Remain Percentage", compute=_onchange_payment_term
+    )
     sign_words = fields.Char(
-        string='Sign Amount Words', compute=_onchange_payment_term)
+        string="Sign Amount Words", compute=_onchange_payment_term
+    )
     amount_words = fields.Char(
-        string='Amount Words', compute=_onchange_payment_term)
+        string="Amount Words", compute=_onchange_payment_term
+    )
     stage_id = fields.Many2one(
-        related='agreement_id.stage_id', string='Agreement Stage')
+        related="agreement_id.stage_id", string="Agreement Stage"
+    )
 
     @api.multi
     def action_generate_template(self):
@@ -120,31 +142,39 @@ class SaleOrder(models.Model):
             if not order.agreement_id:
                 if order.agreement_template_id:
                     name = order.agreement_template_id.name
-                    name += '| ' + order.name
-                    order.agreement_id = order.agreement_template_id.copy(default={
-                        'name': name,
-                        'is_template': False,
-                        'sale_id': order.id,
-                        'partner_id': order.partner_id.id,
-                        'analytic_account_id':
-                            order.analytic_account_id and
-                            order.analytic_account_id.id or False,
-                        'start_date': order.start_date,
-                        'end_date': order.end_date,
-                        'sign_date': order.sign_date
-                    })
+                    name += "| " + order.name
+                    order.agreement_id = order.agreement_template_id.copy(
+                        default={
+                            "name": name,
+                            "is_template": False,
+                            "sale_id": order.id,
+                            "partner_id": order.partner_id.id,
+                            "analytic_account_id": (
+                                order.analytic_account_id
+                                and order.analytic_account_id.id
+                                or False
+                            ),
+                            "start_date": order.start_date,
+                            "end_date": order.end_date,
+                            "sign_date": order.sign_date,
+                        }
+                    )
                     for line in self.order_line:
                         # Create agreement line
-                        self.env['agreement.line'].create({
-                            'product_id': line.product_id.id,
-                            'name': line.name,
-                            'agreement_id': order.agreement_id.id,
-                            'qty': line.product_uom_qty,
-                            'sale_line_id': line.id,
-                            'uom_id': line.product_uom.id
-                        })
+                        self.env["agreement.line"].create(
+                            {
+                                "product_id": line.product_id.id,
+                                "name": line.name,
+                                "agreement_id": order.agreement_id.id,
+                                "qty": line.product_uom_qty,
+                                "sale_line_id": line.id,
+                                "uom_id": line.product_uom.id,
+                            }
+                        )
             if order.agreement_id:
-                order.dynamic_description = order.agreement_id.dynamic_description
+                order.dynamic_description = (
+                    order.agreement_id.dynamic_description
+                )
 
     @api.multi
     def _action_confirm(self):
@@ -167,13 +197,14 @@ class SaleOrder(models.Model):
 
         self.ensure_one()
         action = self.env.ref(
-            'agreement_custom.partner_agreement_contract_document_preview')
+            "agreement_custom.partner_agreement_contract_document_preview"
+        )
         vals = action.read()[0]
-        context = vals.get('context', {})
+        context = vals.get("context", {})
 
-        context['active_id'] = self.agreement_id.id
-        context['active_ids'] = [self.agreement_id.id]
-        vals['context'] = context
+        context["active_id"] = self.agreement_id.id
+        context["active_ids"] = [self.agreement_id.id]
+        vals["context"] = context
         return vals
 
     @api.multi
@@ -181,21 +212,23 @@ class SaleOrder(models.Model):
 
         self.ensure_one()
         action = self.env.ref(
-            'agreement_custom.sale_agreement_contract_document_preview')
+            "agreement_custom.sale_agreement_contract_document_preview"
+        )
         vals = action.read()[0]
-        context = vals.get('context', {})
+        context = vals.get("context", {})
 
-        context['active_id'] = self.id
-        context['active_ids'] = [self.id]
-        vals['context'] = context
+        context["active_id"] = self.id
+        context["active_ids"] = [self.id]
+        vals["context"] = context
         return vals
 
     @api.multi
     def close_agreement_contract(self):
 
         self.ensure_one()
-        stage = self.env['agreement.stage'].search(
-            [('close_stage', '=', True)])
+        stage = self.env["agreement.stage"].search(
+            [("close_stage", "=", True)]
+        )
         if stage:
             stage = stage and stage[0]
             self.agreement_id.stage_id = stage.id
