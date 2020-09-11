28dd18eb1fe7b9ae0ba0502c612575ea625c05f9
paradisocrist <crisparadiso_@hotmail.com>
2020-09-11 13:00:38 -0300

    [fix][customer_invoice_tags] adds translations

--- customer_invoice_tags/models/account_invoice.py
+++ customer_invoice_tags/models/account_invoice.py
@@ -4,10 +4,15 @@ class AccountInvoice(models.Model):
 
     @api.multi
     def action_invoice_open(self):
-        tag_id = self.env.ref('customer_invoice_tags.delay').id 
+        tag_canceled_event_id = self.env.ref('customer_invoice_tags.canceled_event').id 
+        tag_credit_note_id = self.env.ref('customer_invoice_tags.credit_note').id
+        tag_delay_id = self.env.ref('customer_invoice_tags.delay').id 
         for acc in self:
+            if  acc.journal_document_type_id.document_type_id.code == "8" or acc.journal_document_type_id.document_type_id.code == "3":
+                acc.partner_id.category_id = [(4,tag_credit_note_id,0)]
+                acc.partner_id.category_id = [(4,tag_canceled_event_id,0)]
             for rec in acc.journal_id: 
                 if rec.code == "MRO":
-                    acc.partner_id.category_id = [(4,tag_id,0)]
+                    acc.partner_id.category_id = [(4,tag_delay_id,0)]
         res = super(AccountInvoice, self).action_invoice_open()
         return res 
--- customer_invoice_tags/models/account_invoice_refund.py
+++ customer_invoice_tags/models/account_invoice_refund.py
@@ -4,9 +4,11 @@ class AccountInvoiceRefund(models.TransientModel):
 
     @api.multi
     def invoice_refund(self):
-        tag_ids = [self.env.ref('customer_invoice_tags.canceled_event').id ,self.env.ref('customer_invoice_tags.credit_note').id]
+        tag_canceled_event_id = self.env.ref('customer_invoice_tags.canceled_event').id 
+        tag_credit_note_id = self.env.ref('customer_invoice_tags.credit_note').id
         for acc in self:
-            if acc.filter_refund == "cancel":
-                acc.invoice_id.partner_id.category_id = [(6,0,tag_ids)]
+            if (acc.journal_document_type_id.document_type_id.code == "8" or acc.journal_document_type_id.document_type_id.code == "3") and acc.filter_refund != "refund"  :
+                acc.invoice_id.partner_id.category_id = [(4,tag_credit_note_id,0)]
+                acc.invoice_id.partner_id.category_id = [(4,tag_canceled_event_id,0)]
         res = super(AccountInvoiceRefund, self).invoice_refund()
         return res 
