003dc7f6319714f1e719df27556eeb79f1f82291
paradisocrist <crisparadiso_@hotmail.com>
2020-10-23 12:41:32 -0300

    [add][website_quote_report]adds fields to the report

---
+++ website_quote_report/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Website quote report",
+    "version": "11.0.1.1",
+    "author": "Calyx",
+    "category": "Productivity",
+    "license": "LGPL-3",
+    "website": "www.calyxservicios.com.ar",
+    "depends": ["web","website_quote","sale"],
+    "data": [
+        "views/crm_lead_view.xml",
+        "views/crm_event_type_view.xml",
+        "views/sale_order_view.xml",
+        "report/quotation_content.xml",
+    ]
+}
---
+++ website_quote_report/models/__init__.py
@@ -0,0 +1,3 @@
+from . import crm_lead
+from . import crm_event_type
+from . import sale_order
