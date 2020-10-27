04e1609a8e9189169b806ff88c9ac8be9515233c
paradisocrist <crisparadiso_@hotmail.com>
2020-10-27 09:47:20 -0300

    [add][website_quote_report]adds fields to the report

---
+++ website_quote_report/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Website quote report",
+    "version": "11.0.1.1",
+    "author": "Calyx",
+    "category": "Productivity",
+    "license": "LGPL-3",
+    "website": "www.calyxservicios.com.ar",
+    "depends": ["web","website_quote","sale","sale_crm"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/crm_lead_view.xml",
+        "views/crm_event_type_view.xml",
+        "views/sale_order_view.xml",
+        "report/quotation_content.xml",
+    ]
+}
---
+++ website_quote_report/models/crm_lead.py
@@ -0,0 +1,12 @@
+from odoo import models, fields, api, _
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    name_honoree = fields.Char(string='Name of the Honoree')
+
+    event_type_id = fields.Many2one(
+        comodel_name="crm.event.type",
+        string="Event Type",
+    )
+    requested_date = fields.Datetime(string="Request Date")
