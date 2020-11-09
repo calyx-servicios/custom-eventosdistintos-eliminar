144158a7eccfe048cd60d9a6533281d6523ec39d
paradisocrist <crisparadiso_@hotmail.com>
2020-11-09 13:31:58 -0300

    [add][customer_invoice_tags]odoo-338

---
+++ customer_invoice_tags/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom ED Tags',
+    'version': '11.0.1',
+    'category': 'Tools',
+    'author': "Calyx",
+    'website': 'www.calyxservicios.com.ar',
+    'license': 'AGPL-3',
+    'summary': '''Eventos Distintos Tags Customization 1.0''',
+    'depends': [
+        'base',
+        'payment_terms'
+    ],
+    'external_dependencies': {
+    },
+    'data': [
+        'data/data_res_partner.xml'
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
