1f673133b7cf1ec4c5e96a06f0a109625ba64f0c
garaceliguzman <georginaguzman267@gmail.com>
2023-03-16 15:44:10 -0300

    [ODOO-6151][FIX][quotation_date_creation]

--- quotaion_date_creation/__manifest__.py
+++ quotaion_date_creation/__manifest__.py
@@ -13,7 +13,7 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/11.0/odoo/addons/base/module/module_data.xml
     # for the full list
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     # see https://odoo-community.org/page/development-status
     'development_status': 'Production/Stable',
 
--- quotaion_date_creation/models/sale_order.py
+++ quotaion_date_creation/models/sale_order.py
@@ -7,3 +7,7 @@ class SaleOrder(models.Model):
 
     date_create_quotation = fields.Date('Date Create Quotation')
     
+    @api.onchange('requested_date')
+    def _requested_date_in_notes(self):
+        if self.conformation_date:
+            self.note = ('Fecha Solicitada: %s \n') % self.conformation_date + self.template_id.note 
